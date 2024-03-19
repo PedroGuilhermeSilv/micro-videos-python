e252e9b75054e95473e4027dca73e709a6489e5e
guilherme <vinespedro55@gmail.com>
2024-03-19 12:37:22 -0300

    test: :test_tube: Add category API test and update category view to use use case

--- src/django_project/category_app/tests/test_category_api.py
+++ src/django_project/category_app/tests/test_category_api.py
@@ -1,13 +1,35 @@
+from src.core.category.domain.category import Category
 from rest_framework.test import APITestCase
+from django_project.category_app.repository import DjangoORMCategoryRespository
+from django_project.category_app.models import Category as CategoryModel
 
 
 class TestCategoryAPI(APITestCase):
     def test_list_category(self):
-        response = self.client.get("/api/categories/")
-        expected_data = [
-            {"id": 1, "name": "Category 1"},
-            {"id": 2, "name": "Category 2"},
-        ]
+        category_movie = Category(
+            name="Category 1", description="Description 1", is_active=True
+        )
+        category_serie = Category(
+            name="Category 2", description="Description 2", is_active=False
+        )
 
+        repository = DjangoORMCategoryRespository(CategoryModel)
+        repository.save(category_serie)
+        repository.save(category_movie)
+
+        response = self.client.get("/api/categories/")
+        assert response.data == [
+            {
+                "id": str(category_serie.id),
+                "name": category_serie.name,
+                "description": category_serie.description,
+                "is_active": category_serie.is_active,
+            },
+            {
+                "id": str(category_movie.id),
+                "name": category_movie.name,
+                "description": category_movie.description,
+                "is_active": category_movie.is_active,
+            },
+        ]
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, expected_data)
--- src/django_project/category_app/views.py
+++ src/django_project/category_app/views.py
@@ -1,12 +1,30 @@
+from src.core.category.application.use_cases.list_category import (
+    ListCategory,
+    ListCategoryRequest,
+)
 from rest_framework import viewsets
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
+from django_project.category_app.repository import DjangoORMCategoryRespository
+from django_project.category_app.models import Category as CategoryModel
 
 
 class CategoryViewSet(viewsets.ViewSet):
     def list(self, request: Request) -> Response:
-        return Response(
-            status=status.HTTP_200_OK,
-            data=[{"id": 1, "name": "Category 1"}, {"id": 2, "name": "Category 2"}],
-        )
+        dto_input = ListCategoryRequest()
+        repository = DjangoORMCategoryRespository(CategoryModel)
+
+        use_case = ListCategory(repository=repository)
+        output = use_case.execute(dto_input)
+        categories = [
+            {
+                "id": str(category.id),
+                "name": category.name,
+                "description": category.description,
+                "is_active": category.is_active,
+            }
+            for category in output.data
+        ]
+
+        return Response(status=status.HTTP_200_OK, data=categories)
